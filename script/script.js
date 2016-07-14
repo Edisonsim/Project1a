60e0a97df0002671e345ab42089416fb0c483930
Edison Sim <edison_sxw1@hotmail.com>
2016-07-14 17:14:10 +0800

    project-1a-quiz

--- script/script.js
+++ script/script.js
@@ -1,131 +1,120 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-
-  var player1Score = 0;
-  var player2Score = 0;
-
-  var resetButton = document.getElementById("reset");
-  var currQuestion;
+$(document).ready(function() {
+  console.log("ready!");
 
   var questions = [{
     question: "Popeye’s nephews were called Peepeye, Poopeye, Pipeye and Pupeye.",
-    choices: ["true", "false"],
-    answer: 0,
+    answer: "true",
   }, {
     question: "The ‘black box’ in an aeroplane is black.",
-    choices: ["true", "false"],
-    fullAnswer: "It is orange",
-    answer: 1,
+    answer: "false",
   }, {
     question: "Seoul is the capital of North Korea.",
-    choices: ["true", "false"],
-    fullAnswer: "it is the capital of South Korea",
-    answer: 1,
+    answer: "false",
   }, {
     question: "Lightning never strikes in the same place twice",
-    choices: ["true", "false"],
-    fullAnswer: "It strikes in the same place quite a lot.",
-    answer: 1,
+    answer: "true",
   }, {
     question: "Birds are dinosaurs?",
-    choices: ["true", "false"],
-    fullAnswer: "Not all dinosaurs became extinct; some of them survived, and every bird alive today.",
-    anwser: 0,
+    answer: "true",
   }, {
     question: "Coffee originated in Brazil.",
-    choices: ["true", "false"],
-    fullAnswer: "Coffee likely originated in Ethiopia around the 15th century.",
-    answer: 1,
+    answer: "false",
   }, {
     question: "The world’s oldest person to begin primary school was 84 year old?",
-    choices: ["true", "false"],
-    fullAnswer: "Kimani Ng ang a Maruge from Kenya",
-    answer: 0,
+    answer: "true",
   }, {
     question: "Brazil is the biggest producer of cocoa beans.",
-    choices: ["true", "false"],
-    fullAnswer: "The Ivory Coast is the biggest producer of cocoa beans",
-    answer: 1,
+    answer: "true",
   }, {
     question: "Fortune cookies are not Chinese.",
-    choices: ["true", "false"],
-    fullAnswer: "they were invented early 1900s in San Franisco",
-    answer: 0,
+    answer: "false",
   }, {
     question: "Honey can last for at least 1000 years",
-    choices: ["true", "false"],
-    fullAnswer: "Honey doesn't rot",
-    answer: 0,
+    answer: "true",
   }];
 
-
-  // function randomQuestion (question, answers) {
-  //   document.getElementsByClassName("questions")
-  //   this.question = question;
-  //   this.choices = answers;
-  // }
-
-  function displayQuestion() {
-    currQuestion = questions[currentQuestion()];
-    $(".question").html("<h2>" + currQuestion.question + "</h2>");
-
-  }
-
-  displayQuestion();
+  var player1Score = 0;
+  var player2Score = 0;
+  var selectedChoice = 0;
+  var awswerQuestion = 1;
+  var player = 1;
+  var totalAskedQ = 0;
+  var currQnIndex;
 
 
-  //numberOfQuestions()
-  //It should return an integer that is the number of questions in a game
+//============= button on click ===================//
+  $("#start").click(function() {
+    displayQuestion();
+    $("#start").hide();
+  })
+  $(".answer").click(function() {
+
+    incScore(this.id);
+
+
+    //scoreBoard go up each time player 1
+    nextPlayer();
+
+});
+
+
   function currentQuestion() {
-    return Math.floor((Math.random() * 10));
+    var random = Math.floor(Math.random() * questions.length);
+    currQnIndex = questions[random];
+
+    return currQnIndex;
+
   }
-
-  //It should return an integer that is the zero-based index of the current question in the quiz
-  //correctAnswer()
-
-  //It should return an integer that is the zero-based index the correct answer for the currrent question
-  function numberOfQuestions() {
-    return 11;
-    // return quiz.questions.length;
+  function displayQuestion() {
+    currentQuestion()
+    $(".question").html("<h2>" + currQnIndex.question + "</h2>");
+      totalAskedQ++;
   }
-
-  //It should return an integer that is the number of choices for the current question
-  //playTurn(choice)
-
-  function playerTurn(event) {
-    var box = event.target;
-    var statusMsg = document.getElementById("status-msg");
-    //click on box it will switch between player1 or player 2
-    if (box.innerHTML === "" && currentPlayer === "") {
-      currentPlayer = "";
-      statusMsg.innerHTML = "Player 1's turn now.";
+//change displayQuestion each time it switch player
+  function nextPlayer() {
+    if (player === 2) {
+      player = 1;
+      displayQuestion();
       checkWinner();
-
-    } else if (box.innerHTML === "" && currentPlayer === "") {
-      this.innerHTML =
-        currentPlayer = "";
-      statusMsg.innerHTML = "Player 2's turn now.";
+    } else {
+      player = 2;
+      displayQuestion();
       checkWinner();
     }
   }
-  //It should take a single integer, which specifies which choice the current player wants to make. It should return a boolean true/false if the answer is correct.
-  //isGameOver()
 
-  //It should return a true or false if the quiz is over.
-  //whoWon()
+  //Increase curr player score
+  function incScore(selectedAns) {
 
-  //It should return 0 if the game is not yet finished. Else it should return either 1 or 2 depending on which player won. It should return 3 if the game is a draw.
-
-  function restart() {
-    var resetButton = document.getElementById("reset");
-    resetButton.addEventListener('click', function() {
-      location.reload();
-    });
-
-    //It should restart the game so it can be played again.
-    //Assumptions
-
-    //It is assumed that the turns of the player will be automatically changed after each turn.
+    if (selectedAns === currQnIndex.answer) {
+      if (player === 1) {
+        player1Score++;
+        $('#player1').text("Player 1 Score : " + player1Score);
+      } else {
+        player2Score++;
+        $('#player2').text("Player 2 Score : " + player2Score);
+      }
+    }
+  }
+//check playerScore point for winner
+  function checkWinner() {
+      if (totalAskedQ === 10) {
+        if (player1Score > player2Score) {
+          alert("Player 1 wins! Play again?");
+          location.reload();
+        } else if (player2Score > player1Score) {
+          alert("Player 2 wins! Play again?");
+          location.reload();
+        } else {
+          alert("It's a draw!");
+          location.reload();
+        }
+      };
   }
 
-});// end of jquery doc ready
+  $("#reset").click(function() {
+    location.reload();
+  });
+
+});
+// end of jquery doc ready
